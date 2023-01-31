f6d0b24c4d47694056fe9505bbc07541d972735e
Swetha-Baskaran <swethasambothi@gmail.com>
2023-02-01 01:11:28 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import Card from "./Components/card";
+import swetha from "./assets/swetha.jpg";
+import rahul from "./assets/rahul.jpeg";
+import ravi from "./assets/ravi.jpeg";
+import Aos from "aos";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const developers = [
+		{
+			name: "Rahul",
+			img: rahul,
+		},
+		{
+			name: "Swetha",
+			img: swetha,
+		},
+		{
+			name: "Ravi",
+			img: ravi,
+		},
+		{
+			name: "Rahul",
+			img: rahul,
+		},
+		{
+			name: "Swetha",
+			img: swetha,
+		},
+		{
+			name: "Ravi",
+			img: ravi,
+		},
+		{
+			name: "Rahul",
+			img: rahul,
+		},
+		{
+			name: "Swetha",
+			img: swetha,
+		},
+		{
+			name: "Ravi",
+			img: ravi,
+		},
+		{
+			name: "Rahul",
+			img: rahul,
+		},
+		{
+			name: "Swetha",
+			img: swetha,
+		},
+		{
+			name: "Ravi",
+			img: ravi,
+		},
+		{
+			name: "Rahul",
+			img: rahul,
+		},
+		{
+			name: "Swetha",
+			img: swetha,
+		},
+		{
+			name: "Ravi",
+			img: ravi,
+		},
+		{
+			name: "Rahul",
+			img: rahul,
+		},
+		{
+			name: "Swetha",
+			img: swetha,
+		},
+		{
+			name: "Ravi",
+			img: ravi,
+		},
+		{
+			name: "Rahul",
+			img: rahul,
+		},
+		{
+			name: "Swetha",
+			img: swetha,
+		},
+		{
+			name: "Ravi",
+			img: ravi,
+		},
+	];
+	useEffect(() => {
+		Aos.init();
+	}, []);
+	return (
+		<>
+			<h1 style={{textAlign: "center", padding: "2rem 0 1rem 0"}}>PTU Web Team</h1>
+			<p style={{textAlign: "center", paddingBottom: "2rem"}}>Pillars of Puducherry Technological University</p>
+			<div className='App'>
+				{developers.map((e, index) => {
+					return <Card image={e} key={index} />;
+				})}
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/Components/card.jsx
@@ -0,0 +1,91 @@
+import React, {useState} from "react";
+import {AiOutlineArrowRight} from "react-icons/ai";
+import {SlSocialInstagram, SlSocialLinkedin,SlSocialTwitter,SlSocialGithub} from "react-icons/sl";
+import "./style.css";
+import 'aos/dist/aos.css'
+
+const Card = ({image}) => {
+	const [open, setOpen] = useState(false);
+	const handleShowProfile = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<div className='full_card' data-aos="zoom-in" data-aos-offset='200' data-aos-duration='700'  data-aos-anchor-easing='ease-in-out'>
+			<div className='card'>
+				<div className='inner_card outer'>
+					<div className='view_profile' onClick={handleShowProfile}>
+						<div
+							className='material-icons arrow'
+							style={
+								open === true
+									? {transform: "rotate(-140deg)"}
+									: {transform: "rotate(45deg)"}
+							}
+						>
+							<AiOutlineArrowRight />
+						</div>
+					</div>
+					<div className='profile_pic'>
+						<img
+							id='pic'
+							src={image.img}
+							alt="It's my image..."
+						/>
+					</div>
+					<div className='short'>
+						<div className='what_i_am'>
+							<div className='name'>{image.name}</div>
+							<div className='Who_i_am'>web developer | Freelancer</div>
+						</div>
+						<div className='social_media'>
+							<span>
+								<div className='icon'>
+									<SlSocialLinkedin />
+								</div>
+							</span>
+							<span>
+								<div className='icon'>
+									<SlSocialInstagram />
+								</div>
+							</span>
+							<span>
+								<div className='icon'>
+									<SlSocialTwitter />
+								</div>
+							</span>
+							<span>
+								<div className='icon'>
+									<SlSocialGithub />
+								</div>
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className={`${"inner_card inner"} ${open ? "in" : ""}`}>
+					<div className='parts description'>
+						<h4>About me</h4>
+						I'm Maya. A b.tech student specialised on Information
+						Technology. I was fascinated on programming. That made me to
+						learn web development. Now I am a frontend developer working
+						as an intern and also intented to do freelansing works. My
+						desire is to be a full strack developer within a short period
+						of time.
+					</div>
+					<div className='parts skills'>
+						<h4>Skills</h4>
+						<ul>
+							<li>HTML</li>
+							<li>CSS</li>
+							<li>JS</li>
+							<li>PYTHON</li>
+							<li>MYSQL</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
